54813c156d5ff22103b3fcc19b3b3d3d1189ee91
bnuq <31835672+bnuq@users.noreply.github.com>
2022-03-25 14:15:10 +0900

    scipt done

--- Assets/Script/FluidController.cs
+++ Assets/Script/FluidController.cs
@@ -58,7 +58,8 @@ public class FluidController : MonoBehaviour
             [Range(0.1f, 3.0f)]
             public float particleRadius;       // particle 하나의 반지름
 
-            // particle 을 배치할 때, 한 줄에 몇개나 놓을 지, 생성하고자 하는 particle 갯수 지정
+            //particle 을 배치할 때, 한 줄에 몇개나 놓을 지, 생성하고자 하는 particle 갯수 지정
+            //동시에 움직일 수 있는 범위가 되기도 한다
             [Min(2)]
             public int xNum;
             [Min(2)]
@@ -202,37 +203,30 @@ public class FluidController : MonoBehaviour
         int kernelMakeMove;                 //계산된 힘에 맞춰서 이동
         int kernelCheckLimit;               //정해진 범위를 벗어나지 않게 한다
 
-
-
-        // 클릭 시 힘을 주는 커널 <- 삭제 예정
-        int kernelGiveForce;
-
     #endregion
-
-
 
     #region Indirect Draw 관련
 
-    uint[] argsArray = { 0, 0, 0, 0, 0 };
-        ComputeBuffer argsBuffer;
+        //수많은 Particles 를 GPU Instancing 을 통해서 그려낼 것이다
+        //그에 필요한 arguments 정의
+
+        uint[] argsArray = { 0, 0, 0, 0, 0 };
+
+        ComputeBuffer argsBuffer; //argsArray 를 GPU 에 넘기는 데 필요한 버퍼
+        
         Bounds bounds = new Bounds(Vector3.zero, Vector3.one * 0);
 
-
-        // 그리려는 mesh
+        //그리려는 Particle 의 mesh
         public Mesh mesh = null;
 
     #endregion
 
-
-
-
-
     
-    // 1. 가장 먼저 초기화를 진행한다
+    
+    //1. 가장 먼저 초기화를 진행한다
     void Start()
-    {
-        
-        // 카메라 컴포너트 할당 => 메인 카메라를 사용한다
+    { 
+        //카메라 컴포너트 할당 => 메인 카메라를 사용한다
         cam = Camera.main;
 
         // 1-1. 커널들을 연결하고, 필요한 그룹 사이즈를 결정한다
@@ -241,34 +235,33 @@ public class FluidController : MonoBehaviour
         // 1-2. particle 들의 배열과 버퍼를 초기화 한다
         InitArrayAndBuffer();
 
-        // 1-3. 셰이더, 머터리얼에서 필요한 값들을 모두 초기화 해준다
+        //1-3. 셰이더, 머터리얼에서 필요한 값들을 모두 초기화 해준다
         SetPropertiesOnce();
-
     }
-
-
 
 
 
     // 2. 매 프레임마다 처리하는 일
     void Update()
     {        
-        // Wave 진행을 위해, 실행 후 지난 시간을 compute shader 에 넣어주어야 한다
-        // float 의 표현 범위가 10^31 까지 가능하기 때문에, overflow 는 걱정하지 않아도 된다
+        //Wave 진행을 위해, 실행 후 지난 시간을 compute shader 에 넣어주어야 한다
+        //float 의 표현 범위가 10^31 까지 가능하기 때문에, overflow 는 걱정하지 않아도 된다
         totalTime += deltaTime;
         shader.SetFloat("Time", totalTime);
 
         /* 
-            매 프레임, 시간마다 각 particle 의 밀도가 변하므로 밀도를 계산해 주어야 한다
-            이후 밀도로 인한 각 particle 의 압력을 계산해주어야 한다
-            그 다음에는 각 particle 에 가해지는 힘을 구해야 한다
+        전체 프로세스
 
-            밀도 -> 압력 -> Forces
+           매 프레임, 시간마다 각 particle 의 밀도가 변하므로 밀도를 계산해 주어야 한다
+           이후 밀도로 인한 각 particle 의 압력을 계산해주어야 한다
+           그 다음에는 각 particle 에 가해지는 힘을 구해야 한다
+        
+           밀도 -> 압력 -> Forces
 
-            알짜힘을 구했다면, 힘에 의한 가속도를 구하여서 새로운 속도를 구하고 새로운 속도로 이동한 위치를 구한다
-            이후 해당 위치가 정해진 범위를 벗어나지 않게 하면 된다
+           알짜힘을 구했다면, 힘에 의한 가속도를 구하여서 새로운 속도를 구하고 새로운 속도로 이동한 위치를 구한다
+           이후 해당 위치가 정해진 범위를 벗어나지 않게 하면 된다
 
-            가속도 구하고 속도 갱신 위치 갱신 -> 범위 확인
+           가속도 구하고 속도 갱신 위치 갱신 -> 범위 확인
         */
         // 2-1. 각 particle 들의 밀도와 압력을 먼저 계산하고
         shader.Dispatch(kernelComputeDensityPressure, groupSize, 1, 1);
@@ -303,18 +296,8 @@ public class FluidController : MonoBehaviour
                 // 오히려 너무 약한가??
                 inputForce *= power;
 
-
-                // yRange 넣기 전에, 더 작은 값이 x 에 오도록 한다
-                if(yRange.x > yRange.y) 
-                {
-                    float temp = yRange.x;
-                    yRange.x = yRange.y;
-                    yRange.y = temp;
-                }
-
-
-                // compute shader 에 힘이 작용하는 범위와, 힘이 적용될 범위를 보낸다
-                shader.SetVector("yRange", yRange);
+                //compute shader 에 두 마우스 지점 간 높이 차이와, 둘 사이 벡터 값을 넘긴다
+                shader.SetFloat("yDiff", yRange.y - yRange.x);
                 shader.SetVector("inputForce", inputForce);
 
                 
@@ -323,14 +306,8 @@ public class FluidController : MonoBehaviour
             }
 
 
-
-
-
-
-
-
         
-        // 모든 힘에 대한 처리를 끝마치고 나서 ~
+        //모든 힘에 대한 처리를 끝마치고 나서 ~
         // 2-3. 가속도를 구하여서 새로운 속도, 위치를 구한다
         shader.Dispatch(kernelMakeMove, groupSize, 1, 1);
 
@@ -338,8 +315,7 @@ public class FluidController : MonoBehaviour
         shader.Dispatch(kernelCheckLimit, groupSize, 1, 1);
 
 
-
-        // 마지막으로 particle 들을 그려낸다
+        //마지막으로 particle 들을 그려낸다 => GPU Instancing 이용
         Graphics.DrawMeshInstancedIndirect(mesh, 0, material, bounds, argsBuffer);
 
 
@@ -351,89 +327,73 @@ public class FluidController : MonoBehaviour
             Debug.Log("Index 0 is " + temp[0].surfNormal);
             Debug.Log("Index 560 is " + temp[560].surfNormal);
         */
-
-        /* Particle[] temp = new Particle[arraySize];
-        particleBuffer.GetData(temp);
-
-        Debug.Log(temp[0].surfNormal); */
-
+        /* 
+            Particle[] temp = new Particle[arraySize];
+            particleBuffer.GetData(temp);
+            Debug.Log(temp[0].surfNormal);
+        */
     }
 
 
 
-
-
-
-
-    // 1-1. kernel 초기화
+    //1-1. kernel 초기화
     void InitKernel()
     {
-        
-        // particle 의 밀도, 압력을 계산하는 커널
+        //Particle 의 밀도, 압력 값을 계산하는 커널
         kernelComputeDensityPressure = shader.FindKernel("ComputeDensityPressure");
 
-        // 3 가지 종류의 힘의 합을 구하는 커널
+        //기본 3 가지 종류의 힘의 합을 구하는 커널
         kernelComputeForces = shader.FindKernel("ComputeForces");
-        
-        // 가해진 힘을 바탕으로, 속도를 구하고 이동시킨다
+
+        //표면 장력을 계산하는 커널
+        kernelComputeSurfaceForce = shader.FindKernel("ComputeSurfaceForce");
+
+        //마우스 입력을 통해 들어오는 외력을 계산하는 커널
+        kernelComputeInputForce = shader.FindKernel("ComputeInputForce");
+
+        //가해진 힘을 바탕으로, 속도를 구하고 이동시킨다
         kernelMakeMove = shader.FindKernel("MakeMove");
 
         // 정해진 범위를 벗어나지 않게 하는 커널
         kernelCheckLimit = shader.FindKernel("CheckLimit");
 
 
-        // 특정 영역에 있는 particle 들을 한 방향으로 힘을 가하는 커널
-        kernelGiveForce = shader.FindKernel("GiveForce");
-
-
-        // 표면 장력을 계산하는 커널
-        kernelComputeSurfaceForce = shader.FindKernel("ComputeSurfaceForce");
-
-
-        // 마우스 입력을 계산하는 커널
-        kernelComputeInputForce = shader.FindKernel("ComputeInputForce");
-
-
-
+        /*
+        하나의 커널 함수가 사용하는 스레드 그룹 수를 가져온다
+        
+        나는 마찬가지로 모든 커널 함수가 동일한 스레드 그룹 수를 사용하게 했으며
+        x축 방향 스레드만 사용했다
+        */
         uint numThreadsX;
-        // 한 커널의 스레드 그룹 수를 가져온다
-        // 4 종류의 커널 모두 같은 사이즈의 스레드 그룹을 사용한다
         shader.GetKernelThreadGroupSizes(kernelComputeDensityPressure, out numThreadsX, out _, out _);
 
 
-        // 채우고자 하는 갯수를 이용해서, 그리고자 하는 particle 전체 개수를 구한다
+        //각 방향 별 채우고자 하는 Particles 갯수를 이용해서, 그리고자 하는 particle 전체 개수를 구한다
         particleCount = xNum * yNum * zNum;
-
-        
-        // 필요한 최소 그룹의 수
+                
+        //전체 Particles 를 다루는 데 필요한 최소 그룹의 수를 구한다
         groupSize = Mathf.CeilToInt((float)particleCount / (float)numThreadsX);
-
-        
-        // 모든 데이터를 담기 위해 필요한, 배열의 사이즈
+                
+        //모든 데이터를 담기 위해 필요한, 배열의 사이즈 정의
         arraySize = (int)numThreadsX * groupSize;
-        
     }
 
 
 
-    // 1-2. 배열과 버퍼 초기화
+    //1-2. 배열과 버퍼 초기화
     void InitArrayAndBuffer()
     {
-
-        // particle 배열을 만들고 초기화 한다
+        //기본 particle 정보를 저장하는 배열을 만들고 초기화 한다
         particleArray = new Particle[arraySize];
-
-     
+             
         /* 
-            particle 들의 값들을 초기화 해서 담는다
+            particle 들의 위치 값들을 초기화 해서 담는다
             최대한 particle 들이 정해진 영역 내에서, 일정하게 나열되도록 한다
-            그래야 처음에 particle 들이 세팅됐을 때, 지나치게 튕겨나가지 않는다
+            그래야 처음에 particle 들이 세팅됐을 때, 지나치게 튕겨나가지 않는다 => 처음에 여기서 실수해서, 다 튕겨져 나감
         */
-        for (int i = 0; i < particleCount; i++)
-        {
-            
+        for (int i = 0; i < particleCount; i++) { //배열 내 모든 Particles 를 순회
+            //해당 Particle 의 위치
             Vector3 pos = new Vector3();
-
             /* 
                 x -> y -> z 순서로 좌표값을 지정한다
 
@@ -456,137 +416,120 @@ public class FluidController : MonoBehaviour
 
             pos.z = +limitRange.z - (i % zNum) * (2 * limitRange.z + 1) / (zNum - 1) + Random.Range(-seed, seed);
 
-
-            // 초기화 한 particle 을 배열에 넣는다
+            //계산 한 위치 값을 가지는 particle 을 배열에 넣는다
             particleArray[i] = new Particle( pos );
         }
 
 
-
-        // compute buffer 를 초기화 하고 배열 값을 넣는다
+        //compute buffer 를 초기화 하고 초기화한 배열 값을 넣는다
         particleBuffer = new ComputeBuffer(arraySize, Particle_Size);
         particleBuffer.SetData(particleArray);
 
 
-
-        // Indirect Draw 를 위한 argument buffer 도 여기서 초기화 한다
-        
-        // mesh 의 시작 인덱스? 였나?
+        // Indirect Draw 를 위한 argument buffer 도 여기서 초기화 한다       
+        //mesh 를 이루는 인덱스 개수
         argsArray[0] = mesh.GetIndexCount(0);
-        // 그리고자 하는 instance 개수
+        //그리고자 하는 instance 개수
         argsArray[1] = (uint)particleCount;
 
+        //argsArray 를 GPU 로 넘기는 버퍼 만들고 배열 값 할당
         argsBuffer = new ComputeBuffer(1, 5 * sizeof(uint), ComputeBufferType.IndirectArguments);
         argsBuffer.SetData(argsArray);
-
     }
 
 
 
-    // 1-3. 한번만 초기화 해야 하는 값들을 넘긴다
+    //1-3. Shader 가 필요로 하는 변수 값들을 모두 초기화 한다
     void SetPropertiesOnce()
     {
-
-        // 가장 먼저 각 커널들에 compute buffer 를 넘겨준다
+        //가장 먼저 각 커널들에 compute buffer 를 넘겨준다
         shader.SetBuffer(kernelComputeDensityPressure, "particles", particleBuffer);
+
         shader.SetBuffer(kernelComputeForces, "particles", particleBuffer);
+        shader.SetBuffer(kernelComputeSurfaceForce, "particles", particleBuffer);
+        shader.SetBuffer(kernelComputeInputForce, "particles", particleBuffer);
+
         shader.SetBuffer(kernelMakeMove, "particles", particleBuffer);
         shader.SetBuffer(kernelCheckLimit, "particles", particleBuffer);
 
 
-        shader.SetBuffer(kernelGiveForce, "particles", particleBuffer);
-
-        shader.SetBuffer(kernelComputeSurfaceForce, "particles", particleBuffer);
-
-
-        // 마우스 입력 커널에 compute buffer 연결
-        shader.SetBuffer(kernelComputeInputForce, "particles", particleBuffer);
-
-
-
-
-
-        // particle 을 그리기 위한 material 에도 버퍼를 전달한다
+        //particle 을 그리기 위한 material 에도 버퍼를 전달한다
         material.SetBuffer("particles", particleBuffer);
 
 
-
-        // particle 에 대한 정보
+        //particle 에 대한 정보
             shader.SetFloat("particleMass", particleMass);
             shader.SetInt("particleCount", particleCount);
 
             material.SetFloat("particleRadius", particleRadius);
 
         
-        // Smooth Kernel 에 대한 정보
+        //Smooth Kernel 에 대한 정보
             shader.SetFloat("h", smoothingRadius);
             shader.SetFloat("hSquare", smoothingRadius * smoothingRadius);
 
-        
 
         // 압력 계산
             shader.SetFloat("gasCoeffi", gasCoeffi);
             shader.SetFloat("restDensity", restDensity);
 
 
-        // 점성에 의한 힘 계산
-            // 액체의 점성도
-            shader.SetFloat("viscosity", viscosity);
+        //점성에 의한 힘 계산
+            shader.SetFloat("viscosity", viscosity); // 액체의 점성도
 
 
-        // 외력에 의한 힘 계산
+        //외력에 의한 힘 계산
             shader.SetVector("gravityAcel", gravityAcel);
 
 
         // 이동 계산
-            // 시간 간격, 델타 타임
-            shader.SetFloat("deltaTime", deltaTime);
+            shader.SetFloat("deltaTime", deltaTime); // 시간 간격, 델타 타임
 
 
-        // 범위 계산
-            // 공이 움직일 수 있는 범위
-            // 입력받은 limitRange 를 사용한다 => particles 를 담을 크기
-            // compute shader 에서는 limitRange 를 범위를 넘어간느 지를 확인하는 데 사용
-            // 그러니까, 각 영역별로 1 씩 증가시켜서 넘기자
+        //범위 계산
+            /*
+            움직일 수 있는 범위 값, Particles 가 움직일 수 있는 범위
+
+            입력받은 limitRange 를 사용한다 => particles 를 넣는 개수 이기도 하다
+        
+            compute shader 에서는 limitRange 를 움직일 수 있는 범위로 사용
+            그러니까, 각 영역별로 1 씩 증가시켜서 넘겨 => 생성된 이후 충돌이 일어나지 않도록 하자
+            */
             shader.SetVector("limitRange", limitRange + new Vector3(1, 1, 1));
 
             material.SetVector("limitRange", limitRange + new Vector3(1, 1, 1));
 
-
             shader.SetFloat("damping", damping);
 
 
-
-        // Wave 계산
+        //Wave 계산
             shader.SetFloat("extraForce", extraForce);
 
 
-        // Surface
+        //Surface Force
             shader.SetFloat("surfCoeffi", surfCoeffi);
 
-            // compute shader 에서는 surface force 를 계산해야 하니까, 계산에 필요한 임계값이 필요하다
+            //compute shader 에서는 surface force 를 계산해야 하니까, 계산에 필요한 임계값이 필요하다
+            //특정 값을 넘길 때만, 표면 장력을 계산한다
             shader.SetFloats("surfForceThreshold", surfForceThreshold);
 
-
-            // 렌더링에서는 해당 particle 이 표면인지 아닌지를 구분해야 한다
-            // 그리기 위해서 표면을 찾아야 하므로, surfTrackThreshold 값을 shader 에 넣어준다
+            //특정 particle 이 표면인지 아닌지를 구분할 수 있는 값이 있다
+            //surfTrackThreshold 값을 기준으로 해당 Particle 이 표면인지 아닌지를 판단할 수 있다
             material.SetFloat("surfTrackThreshold", surfTrackThreshold);
-            
     }
 
 
     
-
-    // 1-4. 혹시 모르니까, 값이 변할 때마다 셰이더와 머터리얼에 할당해주는 값
+    //1-4. 실행도중 변할 수 있는 값, 변할 때마다 셰이더와 머터리얼에 할당해주어야 한다
     void SetProperties()
     {
         /* Particle */
-            // 그려지는 particle 의 반지름
+            //그려지는 particle 의 반지름
             material.SetFloat("particleRadius", particleRadius);
 
 
         /* Kernel */
-            // smoothingRadius 변화
+            //smoothingRadius 변화
             shader.SetFloat("h", smoothingRadius);
             shader.SetFloat("hSquare", smoothingRadius * smoothingRadius);
 
@@ -597,7 +540,7 @@ public class FluidController : MonoBehaviour
 
 
         /* Viscosity */
-            // viscosity 변화
+            //viscosity 변화
             shader.SetFloat("viscosity", viscosity);
 
 
@@ -610,26 +553,20 @@ public class FluidController : MonoBehaviour
             shader.SetFloats("surfForceThreshold", surfForceThreshold);
 
             material.SetFloat("surfTrackThreshold", surfTrackThreshold);
-
     }
 
 
-
+    //Inspector 창에서 값이 변할 때마다, 자동으로 호출되는 함수
     void OnValidate()
     {
         SetProperties();
     }
 
-
-
-
-
     
-    // 파괴 시, 모든 compute buffer 삭제
+    //프로그램 종료 시, 모든 compute buffer 삭제
     void OnDestroy()
     {
         particleBuffer.Dispose();
         argsBuffer.Dispose();
     }
-
 }
